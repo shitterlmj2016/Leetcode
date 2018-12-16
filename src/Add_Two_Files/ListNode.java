34f3a4e9b48e0f4a4e12eef2e123d671ec59fed9
xinchenh <915930987@qq.com>
2018-12-15 21:26:56 -0500

    2.List Node

--- src/Add_Two_Files/ListNode.java
+++ src/Add_Two_Files/ListNode.java
@@ -13,17 +13,40 @@ public class ListNode {
         val = x;
     }
 
+//Suggest
+    ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+
+
     public static void main(String[] args) {
-        ListNode m1 = new ListNode(9);
-        ListNode m2 = new ListNode(9);
+        ListNode m1 = new ListNode(5);
+//        ListNode m2 = new ListNode(4);
 //        ListNode m3 = new ListNode(3);
 
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(6);
+        ListNode n1 = new ListNode(5);
+//        ListNode n2 = new ListNode(6);
 //        ListNode n3 = new ListNode(4);
-       m1.next = m2;
+//        m1.next = m2;
 //        m2.next = m3;
-        n1.next = n2;
+//        n1.next = n2;
 //        n2.next = n3;
 
         solution s = new solution();
@@ -37,35 +60,44 @@ public class ListNode {
 
 class solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack l1Stack = new Stack<Integer>();
-        Stack l2Stack = new Stack<Integer>();
+        String s1 = "";
+        String s2 = "";
         while (l1 != null) {
-            l1Stack.push(l1.val);
+            s1 = s1 + l1.val;
             l1 = l1.next;
 
         }
 
-        while(l2!=null)
-        {l2Stack.push(l2.val);
+        while (l2 != null) {
+            s2 = s2 + l2.val;
             l2 = l2.next;
         }
 
+        int a=s1.length();
+        int b=s2.length();
+        //??????????????????????????
+        int length=a;
+        if (a > b) {
+            for (int i = 0; i < a - b; i++) {
+                s2 = s2 + "0";
+            }
+        } else {
+            length=b;
+            for (int i = 0; i < b - a; i++) {
+                s1 = s1 + "0";
+            }
+        }
 
-        int length=l1Stack.size();
-        if (l2Stack.size()>length)
-            length = l2Stack.size();
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        return null;
 
         String s="";
         boolean jinwei = false;
         for(int i=0; i< length;i++)
         {
-            int m=0,n=0;
-            if (l1Stack.size()!=0) {
-                m = (int)l1Stack.pop();
-            }
-            if (l2Stack.size()!=0) {
-                n = (int)l2Stack.pop();
-            }
+            int m=s1.charAt(i)-48;
+            int n=s2.charAt(i)-48;
 
             int j=m+n;
 
@@ -87,7 +119,7 @@ class solution {
 
         if (jinwei)
         {
-            s="1"+s;
+            s=s+"1";
         }
 
         System.out.println(s);
@@ -95,7 +127,7 @@ class solution {
 
        char [] answer=s.toCharArray();
 
-       ListNode result = new ListNode(answer[answer.length-1]-48);
+       ListNode result = new ListNode(answer[0]-48);
 
         if(answer.length==1)
         {
@@ -110,7 +142,7 @@ class solution {
 
             ListNode next = new ListNode(0);
             pointer.next = next;
-            next.val = answer[answer.length-1-i]-48;
+            next.val = answer[i]-48;
             pointer = pointer.next;
 
        }
@@ -118,3 +150,4 @@ class solution {
        return result;
     }
 }
+
